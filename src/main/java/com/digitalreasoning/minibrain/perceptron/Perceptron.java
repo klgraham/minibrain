f9c9932f0cf12dc5c5c9a90108e1878ac989c651
Kenneth Graham <klgraham@gmail.com>
2015-12-27 16:24:38 -0500

    initial commit

---
+++ src/main/java/com/digitalreasoning/minibrain/perceptron/Perceptron.java
@@ -0,0 +1,46 @@
+package com.digitalreasoning.minibrain.perceptron;
+
+import Jama.Matrix;
+
+/**
+ * Created by klogram on 12/27/15.
+ */
+public class Perceptron {
+    // binary inputs x_0, ..., x_{N-1}
+    public double[] inputs; // only one row exists
+
+    public int N;
+
+    // weights
+    public double[] weights; // only one row exists
+
+    public double threshold;
+
+    public Perceptron(double[] inputs, double[] weights, double threshold) {
+        N = inputs.length;
+        this.inputs = inputs;
+        this.weights = weights;
+        this.threshold = threshold;
+    }
+
+    public Integer output()
+    {
+        double sum;
+        Matrix x = new Matrix(inputs, 1);
+        Matrix w = new Matrix(weights, 1);
+        x.print(1,3);
+        Matrix xDotX = x.times(w.transpose());
+        sum = xDotX.get(0,0) - threshold;
+        return sum > 0 ? 1 : 0;
+    }
+
+    public static void main(String[] args)
+    {
+        double[] binaryInputs = {1, 0, 1};
+        double[] weights = {6, 2, 2};
+        double threshold = 10;
+        Perceptron perceptron = new Perceptron(binaryInputs, weights, threshold);
+        double output = perceptron.output();
+        System.out.println(output);
+    }
+}
