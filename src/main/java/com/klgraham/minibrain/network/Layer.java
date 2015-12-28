68a531f3e52543117e10bcf27c4de59891c313b7
Kenneth Graham <klgraham@gmail.com>
2015-12-28 12:24:47 -0500

    A single input vector can now have multiple features.

--- src/main/java/com/klgraham/minibrain/network/Layer.java
+++ src/main/java/com/klgraham/minibrain/network/Layer.java
@@ -8,37 +8,69 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 /**
- * A layer is an array of Neurons
+ * Represents a layer of neurons in a neural network.
+ *
  * Created by klogram on 12/27/15.
  */
 public class Layer
 {
     List<Neuron> neurons;
     double biasUnit = 1.0;
+
+    /**
+     * Number of neurons in the layer.
+     */
     int numberOfNeurons;
+
+    /**
+     * Number of inputs to the layer, and to each neuron in the layer.
+     * This is the number of columns in the input matrix.
+     */
     int numberOfInputs;
+
+    /**
+     * Number of features per input. This is also the number of rows in the
+     * input matrix.
+     */
+    int numberOfFeatures;
+
     ActivationFunction f;
 
-    private Layer(int numberOfNeurons, int numberOfInputs, ActivationFunction f)
+    private Layer(final int numberOfNeurons, final int numberOfInputs, final int numberOfFeatures, ActivationFunction f)
     {
         this.numberOfNeurons = numberOfNeurons;
         this.numberOfInputs = numberOfInputs;
+        this.numberOfFeatures = numberOfFeatures;
         this.f = f;
         neurons = new ArrayList<>(numberOfNeurons);
     }
 
-    public Layer init(int numberOfNeurons, int numberOfInputs, ActivationFunction f)
+    /**
+     * Creates a layer of neurons, with weights initialized on [0, 1].
+     * @param numberOfNeurons
+     * @param numberOfInputs
+     * @param numberOfFeatures
+     * @param f
+     * @return Layer of neurons
+     */
+    public Layer init(final int numberOfNeurons, final int numberOfInputs, final int numberOfFeatures, ActivationFunction f)
     {
-        Layer layer = new Layer(numberOfNeurons, numberOfInputs, f);
+        Layer layer = new Layer(numberOfNeurons, numberOfInputs, numberOfFeatures, f);
+
         IntStream.range(1, numberOfNeurons).forEach(i -> {
             Neuron n = new Neuron(0, f);
-            n.initRandom(numberOfInputs);
+            n.initRandom(numberOfInputs, numberOfFeatures);
             neurons.add(n);
         });
         return layer;
     }
 
-    public double[] process(double[] inputs)
+    /**
+     * Computes the output of the entire layer.
+     * @param inputs
+     * @return
+     */
+    public double[] process(double[][] inputs)
     {
         double[] outputs = new double[numberOfNeurons];
         IntStream.range(0, numberOfNeurons-1).forEach(i -> {
--- src/main/java/com/klgraham/minibrain/neuron/Neuron.java
+++ src/main/java/com/klgraham/minibrain/neuron/Neuron.java
@@ -6,37 +6,55 @@ import java.util.Random;
 import java.util.function.Function;
 
 /**
+ * A Neuron has N inputs, each of which is a vector of size K, and produces a single output value,
+ * as determined by the activation function. For each input, there is a corresponding vector of weights.
+ *
  * Created by klogram on 12/27/15.
  */
 public class Neuron
 {
-    public double[] weights;
+    /**
+     * K x N matrix of weights. Each column contains the weight vector for the corresponding input vector.
+     */
+    public double[][] weights;
+
     public double bias;
     private double output;
+
+    /**
+     * Activation function h_{w,b}(x),
+     * where x = (x_0, ..., x_{N-1}), each x_i is a column matrix
+     */
     private Function<Double, Double> f;
 
-    public Neuron(double[] w, double bias, ActivationFunction f)
+
+    public Neuron(double[][] w, final double bias, ActivationFunction f)
     {
         this.weights = w;
         this.bias = bias;
         this.f = f.get();
     }
 
-    public Neuron(double bias, ActivationFunction f)
+    public Neuron(final double bias, ActivationFunction f)
     {
         this.bias = bias;
         this.f = f.get();
         this.weights = null;
     }
 
-    private double z(double[] inputs)
+    private double z(final double[][] inputs)
     {
-        Matrix x = new Matrix(inputs, 1);
-        Matrix w = new Matrix(weights, 1);
-        return w.times(x.transpose()).trace() + bias;
+        Matrix x = new Matrix(inputs);
+        Matrix w = new Matrix(weights);
+        return w.transpose().times(x).trace() + bias;
     }
 
-    public double process(double[] inputs)
+    /**
+     * Computes the output of the Neuron.
+     * @param inputs
+     * @return Neuron's output value.
+     */
+    public double process(final double[][] inputs)
     {
         output = f.apply(z(inputs));
         return output;
@@ -44,8 +62,8 @@ public class Neuron
 
     public static void main(String[] args)
     {
-        double[] inputs = {1, 0, 1};
-        double[] weights = {6, 2, 2};
+        double[][] inputs = {{1, 0, 1}};
+        double[][] weights = {{6, 2, 2}};
         double bias = 10;
         Neuron neuron = new Neuron(weights, bias, ActivationFunction.SIGMOID);
         neuron.process(inputs);
@@ -56,23 +74,25 @@ public class Neuron
         return output;
     }
 
-    public void init(int n)
+    public void init(final int numInputs, final int numFeatures)
     {
-        this.weights = new double[n];
-        for (int i = 0; i < n; i++)
-        {
-            weights[i] = 0.0;
+        this.weights = new double[numFeatures][numInputs];
+        for (int i = 0; i < numInputs; i++) {
+            for (int j = 0; j < numFeatures; j++) {
+                weights[i][j] = 0.0;
+            }
         }
     }
 
     private Random r = new Random();
 
-    public void initRandom(int n)
+    public void initRandom(final int numInputs, final int numFeatures)
     {
-        this.weights = new double[n];
-        for (int i = 0; i < n; i++)
-        {
-            weights[i] = r.nextDouble();
+        this.weights = new double[numFeatures][numInputs];
+        for (int i = 0; i < numInputs; i++) {
+            for (int j = 0; j < numFeatures; j++) {
+                weights[i][j] = r.nextDouble();
+            }
         }
     }
 }
